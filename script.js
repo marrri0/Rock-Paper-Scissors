5babce873023b0dc9d8567e82e7b9bc9ffe76390
mario <marwanlamalmi100@gmail.com>
2023-05-16 19:28:14 +0100

    final update

--- script.js
+++ script.js
@@ -1,30 +1,75 @@
 const para = document.getElementById('para');
-const rock = document.getElementById('rock-btn');
-const paper = document.getElementById('paper-btn');
-const scissors = document.getElementById('scissors-btn');
 const resetBtn = document.querySelector('.reset-btn');
+const playBtn = document.querySelectorAll('.selection');
 
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
-let rockCounter = 0;
-let paperCounter = 0;
-let scissorsCounter = 0;
-
-function updateRock() {
-    rockCounter++
-    para.innerHTML = `You clicked Rock ${rockCounter} times`
-}
-
-function updatePaper() {
-    paperCounter++
-    para.innerHTML = `You clicked Paper ${paperCounter} times`
-}
-
-function updateScissors() {
-    scissorsCounter++
-    para.innerHTML = `You clicked Scissors ${scissorsCounter} times`
-}
-
+// reload func
 resetBtn.addEventListener('click', () => location.reload());
-rock.addEventListener('click', updateRock);
-paper.addEventListener('click', updatePaper);
-scissors.addEventListener('click', updateScissors);
+
+//player choice
+playBtn.forEach((selection) => {   
+    selection.addEventListener('click', ()=> {
+        let playerChoice = selection.querySelector('img');
+        playerSelection = playerChoice.alt;
+        game();
+        if (playerScore === 5 || computerScore === 5) {
+            disableButtons();
+        };
+    });
+});
+
+// computer choice
+function getComputerChoice () {
+    let choices = ["rock", "paper", "scissors"];
+    let index = Math.floor(Math.random() * choices.length)
+    return choices[index];  
+}; 
+
+//play round
+function playRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
+    
+    if (playerSelection === computerSelection) {
+        return `Tie Game!`
+    } else if (
+        (computerSelection === "rock" && playerSelection === "paper" )||
+        (computerSelection === "paper" && playerSelection === "scissors")||
+        (computerSelection === 'scissors' && playerSelection === 'rock')
+        ) {
+            playerScore++;
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        } else {
+            computerScore++;
+            return `You Lose! ${computerSelection} beats ${playerSelection}`
+        };
+}; 
+
+function game() {
+    let RoundGame = para;
+    let score = scorePanel;
+
+    RoundGame.textContent = playRound(playerSelection, computerSelection);
+    score.innerHTML = `HUMANITY ${playerScore} - ${computerScore} MACHINES`;
+
+    if (playerScore === 5) {
+        para.innerHTML = `ALL HAIL THE NEW CHAMP!`
+        score.style.color = 'yellow'
+        RoundGame.style.color = 'yellow'
+        RoundGame.style.textShadow = "0px 0px 15px #fff"
+    } else if (computerScore === 5) {
+        para.innerHTML = `DID YOU JUST LOSE TO A MACHINE? UGH...`;
+        score.style.color = 'red'
+        RoundGame.style.color = 'red'
+        RoundGame.style.textShadow = "0px 0px 15px #fff"
+    }
+}; 
+// disables the choice buttons when the game reaches its goal
+function disableButtons () {
+    playBtn.forEach((selection) => {
+        selection.disabled = true
+    });
+};
